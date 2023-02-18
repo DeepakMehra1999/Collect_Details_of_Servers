954c031387065583fec8ae3e7a9a58822df7043c
DeepakMehra1999 <48806909+DeepakMehra1999@users.noreply.github.com>
2023-02-18 20:15:47 +0530

    Add files via upload

---
+++ python_script_to_edit_table.py
@@ -0,0 +1,118 @@
+#importing some important python modules
+import sys
+import os
+import csv
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from prettytable import PrettyTable
+import subprocess
+import socket
+
+#Getting the hostname of the system
+hostname = socket.gethostname()
+
+master_sheet_file_name="master_sheet"
+#Creating a filename for the csv file using the hostname
+master_sheet = "{}.csv".format(master_sheet_file_name)
+
+#Changing the current working directory to a specific folder
+#os.chdir('/home/sa-jan-mvp/overall-monitoring-report')
+os.chdir('/home/sa-jan-mvp/Testing')
+#present working directory
+pwd=os.getcwd()
+
+with open(master_sheet, "w") as outfile:
+    header_written = False
+    for filename in os.listdir():
+        if filename.endswith(".csv"):
+            with open(filename, "r") as infile:
+                for line in infile:
+                    if not header_written:
+                        outfile.write(line)
+                        header_written = True
+                    else:
+                        outfile.write(line)
+
+
+
+#Initializing a table header using the 'prettytable' module
+table_header = PrettyTable()
+table_header.field_names = ["Account", "Hostname", "Disk_Space", "Total_Memory", "Free_Memory", "Free_Memory_Status", "Service", "Uptime", "Comment"]
+
+#Reading the data from the csv file
+with open(master_sheet, "r") as file:
+    header = file.readline()
+    for line in file:
+        if line.strip() == header.strip():
+            continue
+        values = line.strip().split("\t")
+        table_header.add_row(values)
+
+print(table_header)
+
+try:
+    my_message1 = table_header.get_html_string()
+
+    html1 = """\
+    <html>
+        <head>
+            <style>
+                table{
+                    font-size:12px;
+                    color: #333333;
+                    border-width: 1px;
+                    border-color: #000000;
+                    border-collapse: collapse;
+                }
+                th {
+                    font-size:15px;
+                    background-color:#acc8cc;
+                    border-width: 1px;
+                    padding: 8px;
+                    border-style: solid;
+                    border-color: #000000;
+                    text-align:left;
+                }
+                tr {
+                    background-color: #FFFFFF;
+                }
+                td {
+                    font-size:13px;
+                    border-width: 1px;
+                    padding: 8px;
+                    border-style: solid;
+                    border-color: #8c8c8c;
+                    text-align:center;
+                    color: #000000;
+                    font-weight: bold;
+                }
+            </style>
+        </head>
+        <body>
+            <table>
+               <br><br> %s
+            </table>
+        </body>
+    </html>
+    """ %(my_message1)
+    #part1 = MIMEText(text, 'plain')
+    part2 = MIMEText(html1, 'html')
+    msg = MIMEMultipart('alternative')
+    #msg.attach(part1)
+    msg.attach(part2)
+    #Converting the table to a string representation
+    #table_string = part2.as_string()
+    table_string = part2.as_string().split('\n\n', 1)[1]
+    #Running a shell script to send an email
+    with open("output.html", "w") as f:
+        f.write(table_string)
+
+    os.chdir(pwd)
+    bash_script_path = "./mailing.sh"
+    subprocess.call(["bash", bash_script_path])
+    #os.remove(pwd + "/" + master_sheet)
+    #os.remove(pwd + "/" + output.html)
+
+except Exception as e:
+    print('Something went wrong...')
+    print(e)
---
+++ script_to_send_mail.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+#Getting the hostname of the system
+host=$(hostname)
+
+#present working directory
+pwd=$(pwd)
+
+#Counting the number of records in the csv file
+master_sheet_records_count=$(cat master_sheet.csv | awk '(NR > 1)' | wc -l)
+
+#fetching master_sheet details
+master_sheet_details=$(cat master_sheet.csv)
+#formatting master_sheet details for email
+email_body=$(echo "$master_sheet_details" | awk -F '\t' '{printf "%-15s%-25s%-15s%-22s%-20s%-20s%-25s%-25s\n", $1, $2, $3, $4, $5, $6, $7, $8, $9}')
+
+
+message_body1="Hi All,\nDaily Health check is done for $master_sheet_records_count server(s) ,Please find the results below. \n"
+message_body2="Condition of Breach :-"
+message_body3="\tServices: Actual number of services not matching the expected number of services.\n\tDisk Space: Disk Space in the respective directory exceeds 80%.\n\tFree Memory: Free Memory in the respective directory goes below to 20% of Total Memory.\n"
+message_body4="Thank you,\nLumin Ops Support"
+
+# Send the email using the mutt command
+# Set the subject of the email
+subject="Daily Health Check Report"
+recipient="email_id_!@xyz.com email_id_2@xyz.com "
+
+#Set the path of the HTML file to attach
+html_file="$pwd/output.html"
+#echo -e "$message_body1\n$email_body\n\n$message_body2" | mail -s "J&J Daily Health Check Report " -a "$html_file" $recipient
+
+v=`date +%H`
+Breached_check=$(cat master_sheet.csv | awk '(NR>1)' | grep Breached | wc -l)
+
+if [ $Breached_check -ge 1 ]; then
+    echo -e "$message_body1\n\n$master_sheet_details\n\n$message_body2\n$message_body3\n\n$message_body4" | mail -s "J&J Daily Health Check Report"  -a "$html_file" $recipient
+else
+    if [[ "$v" -gt 20 ]] && [[ "$v" -lt 22 ]]; then
+        echo -e "$message_body1\n\n$master_sheet_details\n\n$message_body2\n$message_body3\n\n$message_body4" | mail -s "J&J Daily Health Check Report"  -a "$html_file" $recipient
+        exit 1;
+    else
+        if [[ "$v" -gt 5 ]] && [[ "$v" -lt 7 ]]; then
+            echo -e "$message_body1\n\n$master_sheet_details\n\n$message_body2\n$message_body3\n\n$message_body4" | mail -s "J&J Daily Health Check Report"  -a "$html_file" $recipient
+            exit 1;
+        else
+            if [[ "$v" -gt 11 ]] && [[ "$v" -lt 13 ]]; then
+                echo -e "$message_body1\n\n$master_sheet_details\n\n$message_body2\n$message_body3\n\n$message_body4" | mail -s "J&J Daily Health Check Report"  -a "$html_file" $recipient
+                exit 1;
+            else
+                exit 1;
+            fi
+        fi
+    fi
+fi
